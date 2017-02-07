f6ab6ed0845dea2d3f257abdcd0b77a97a7394ce
lindsaylevine <lindsay.levine@venmo.com>
2017-02-06 22:53:52 -0500

    add index.js and README with basic dependencies

---
+++ README.md
@@ -0,0 +1,6 @@
+# scholarship
+
+## running the app
+1. npm install
+2. npm start
+3. -> localhost:3000
---
+++ index.js
@@ -0,0 +1,17 @@
+import { emoji } from 'node-emoji'
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.status(200).send('hello scholarship'));
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(400).send(err.message);
+});
+
+app.listen(port, () => console.log(`${emoji.ear} listenin on ${port} ${emoji.ear}`));
