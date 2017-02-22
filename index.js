0ac3968b2b3b451b8e5f51d99202de34e2611a90
lindsaylevine <lindsay.levine@venmo.com>
2017-02-21 20:04:59 -0500

    FE + BE scaffolding

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 import { emoji } from 'node-emoji'
 import express from 'express';
 import bodyParser from 'body-parser';
-
-import { scholars } from './routes/scholars';
+import cors from 'cors';
+import path from 'path';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-
-app.get('/', (req, res) => res.status(200).send('hello scholarship'));
-
-app.use('/scholars', scholars);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.static(path.normalize(__dirname + '/dist')));
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
@@ -19,3 +18,11 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(port, () => console.log(`${emoji.ear} listenin on ${port} ${emoji.ear}`));
+
+import { addRoutes } from './api/endpoints';
+addRoutes(app);
+
+app.get('*', (req, res) => {
+  res.sendFile('dist/index.html', { root: __dirname });
+});
+
