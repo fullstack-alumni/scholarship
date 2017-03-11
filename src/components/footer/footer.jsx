f94d51134343ce7e1c74cd440448a12840d4c6f5
Emily H <emilyyho33@gmail.com>
2017-03-11 18:10:34 -0500

    footer component added

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default class Footer extends React.PureComponent {
+  render() {
+    return (
+      <div className="footer container">
+        <div className="row">
+          <div className="col-sm-12 col-md-4 info-link">
+            <div>
+              Want to Sponser?
+            </div>
+            <div>
+              Have questions?
+            </div>
+            <div>
+              team@alumni.com
+            </div>
+          </div>
+          <div className="col-sm-12 col-md-4 contact-link">
+            <div>
+              about
+            </div>
+            <div>
+              contribute
+            </div>
+            <div>
+              apply
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
