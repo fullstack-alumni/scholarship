7750287e63389183592ae7617feca493897fc693
Joe Alves <joe@fullstackacademy.com>
2017-03-23 11:00:27 -0400

    Defaults for database connection (name: "scholarship", user: "", pass: "", port: 5432, host: "localhost") if vagrant isnt used

--- database/_db.js
+++ database/_db.js
@@ -5,11 +5,11 @@ const {
 } = process.env;
 
 export default new Sequelize(
-  DB_NAME, DB_USER, DB_PASS,
+  DB_NAME || 'scholarship', DB_USER || '', DB_PASS || '',
   {
     dialect: 'postgres',
-    port: DB_PORT,
-    host: DB_HOST,
+    port: DB_PORT || 5432,
+    host: DB_HOST || 'localhost',
     logging: false
   }
 );
