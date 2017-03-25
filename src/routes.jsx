8a4d70efbdebc776ffed4e91f2cf8329933579eb
Colin VanLang <colinvanlang@gmail.com>
2017-03-25 16:06:02 -0400

    add login page

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import Home from 'containers/home';
+import Login from 'components/login';
 
 const routes = [
-  <Route path="/" component={Home} />,
+  <Route exact path="/" component={Home} />,
+  <Route path="/login" component={Login} />,
 ];
 
 export default routes;
