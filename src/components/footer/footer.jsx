ec8259a1eb8901e83a262117389383e53d2798ce
Colin VanLang <colinvanlang@gmail.com>
2017-03-25 16:33:54 -0400

    add row div back

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,26 +5,28 @@ export default class Footer extends React.PureComponent {
     return (
       <div className="footer">
         <div className="container">
-          <div className="col-sm-12 col-md-2 info-link">
-            <div className="sponser">
-              Want to Sponser?
+          <div className="row">
+            <div className="col-sm-12 col-md-2 info-link">
+              <div className="sponser">
+                Want to Sponser?
+              </div>
+              <div className="questions">
+                Have questions?
+              </div>
+              <div className="email">
+                team@alumni.com
+              </div>
             </div>
-            <div className="questions">
-              Have questions?
-            </div>
-            <div className="email">
-              team@alumni.com
-            </div>
-          </div>
-          <div className="col-sm-12 col-md-2 contact-link">
-            <div>
-              About
-            </div>
-            <div>
-              Contribute
-            </div>
-            <div>
-              Apply
+            <div className="col-sm-12 col-md-2 contact-link">
+              <div>
+                About
+              </div>
+              <div>
+                Contribute
+              </div>
+              <div>
+                Apply
+              </div>
             </div>
           </div>
         </div>
