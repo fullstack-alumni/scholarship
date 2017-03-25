c411378c8c23dcc214f70c23011dd4abfb22e8eb
Colin VanLang <colinvanlang@gmail.com>
2017-03-25 18:22:50 -0400

    add sign up page

--- src/components/sign_up.jsx
+++ src/components/sign_up.jsx
@@ -3,29 +3,19 @@ import React from 'react';
 import Footer from 'components/footer/footer';
 import Form from 'components/form';
 import TitleBar from 'components/title_bar/title_bar';
-import { applyItem } from 'components/title_bar/title_bar_item';
+import { applyItem, loginItem } from 'components/title_bar/title_bar_item';
 
 export default class Login extends React.PureComponent {
-  getExtraText() {
-    return (
-      <p>
-        <a href="/signup">Sign up</a> as a doner<br />
-        <a href="/apply">Apply</a> for a scholarship
-      </p>
-    );
-  }
-
   render() {
     return (
       <div>
-        <TitleBar rightItems={[applyItem]} />
+        <TitleBar rightItems={[loginItem, applyItem]} />
         <div className="body-with-title-bar container">
           <div className="row">
             <div className="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-12 text-center">
               <Form
-                title="Doner?"
-                buttonText="See Who You're Helping"
-                extraInfo={this.getExtraText()}
+                title="Sign Up to Donate"
+                buttonText="Change a Life"
               />
             </div>
           </div>
