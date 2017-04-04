6576efd4aca605579e6e9350d094dd9c84696bc4
Emily H <emilyyho33@gmail.com>
2017-04-04 18:41:27 -0400

    Apply with info component created and added to routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,11 +4,13 @@ import { Route } from 'react-router-dom';
 import Home from 'containers/home';
 import Login from 'components/login';
 import SignUp from 'components/sign_up';
+import Apply from 'components/apply';
 
 const routes = [
   <Route exact path="/" component={Home} />,
   <Route path="/login" component={Login} />,
   <Route path="/signup" component={SignUp} />,
+  <Route path="/apply" component={Apply} />
 ];
 
 export default routes;
